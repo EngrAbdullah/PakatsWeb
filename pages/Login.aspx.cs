3d60e775975c77fe5ce64a46add09c224858b4df
EngrAbdullah <pakatsprojects@gmail.com>
2014-06-30 12:40:11 +0500

    commit

--- pages/Login.aspx.cs
+++ pages/Login.aspx.cs
@@ -15,13 +15,53 @@ namespace pakatsWeb.pages
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["UserName"] != null)
+            {
+
+                Response.Redirect("userPage.aspx");
+            }
 
         }
 
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
-            
+            Session["UserName"] = null;
+
+
+            double userid = 0;
+            userid = (from c in db.login_tb where c.userName == this.txtUserName.Text && c.password == this.txtPassword.Text select c.id).FirstOrDefault();
+
+            if (userid != 0)
+            {
+                if (this.txtlogId.Text == "admin")
+                {
+                    Session["UserName"] = "Welcome " + login;
+                    Session["loginName"] = "admin";
+                    Session["UserId"] = userid;
+
+                    Response.Redirect("..\\admin\\sys_admin_123@sina.aspx");
+                    // errormsg.Text = "Admin";
+
+                }
+                else
+                {
+                    Session["UserName"] = "Welcome " + login;
+
+                    Session["loginName"] = null;
+                    Session["UserId"] = userid;
+                    Response.Redirect("userPage.aspx");
+                    // errormsg.Text = "Success";
+                }
+
+            }
+            else
+            {
+                errormsg.Visible = true;
+                errormsg.Text = "Login Failed Check User Name And password";
+
+            }
         }
+
 
 
     }
