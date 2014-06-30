6781e3a72d34b25116d2df76a22ea6814ff52cb9
EngrAbdullah <pakatsprojects@gmail.com>
2014-06-30 11:55:26 +0500

    First Upload
    This is the first time Uploading remember to change the password and the
    username in database

---
+++ pages/Login.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/masterPage.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="pakatsWeb.pages.WebForm1" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <table>
+        <tr>
+            <td>
+                <asp:Label ID="lblUserName" runat="server" Text="User Name"></asp:Label>
+
+            </td>
+            <td>
+                <asp:TextBox ID="txtUserName" runat="server"></asp:TextBox>
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <asp:Label ID="lblPassword" runat="server" Text="Password"></asp:Label>
+
+            </td>
+            <td>
+                <input id="txtPassword" type="password" />
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+
+            </td>
+            <td>
+                <asp:Button ID="btnSubmit" runat="server" Text="Submit" OnClick="btnSubmit_Click" />
+            </td>
+        </tr>
+    </table>
+
+
+</asp:Content>
---
+++ pages/Login.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+namespace pakatsWeb.pages
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string connetionString = null;
+        SqlCommand cmd;
+        SqlDataReader dataReader;
+        SqlConnection cnn;
+        string query = "";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+           
+            connetionString = "Data Source=PAKATS-PC;Initial Catalog=pakats_db;User ID=sa;Password=pakats";
+            cnn = new SqlConnection(connetionString);
+            try
+            {
+                query = "";
+                cmd = new SqlCommand(query, cnn);
+                if (cnn.State != ConnectionState.Open)
+                    cnn.Open();
+                dataReader = cmd.ExecuteReader();
+                dataReader.Close();
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                lblPassword.Text=ex.ToString();
+            }
+        }
+
+
+    }
+}
